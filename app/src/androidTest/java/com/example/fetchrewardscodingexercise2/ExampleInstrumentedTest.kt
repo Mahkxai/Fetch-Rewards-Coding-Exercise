8475db596ab9d02eaf3e519ae768ebe286ee11ee
Mahkxai <shr.hardik@gmail.com>
2023-10-30 14:12:55 -0400

    Version1 Saved

--- app/src/androidTest/java/com/example/fetchrewardscodingexercise2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/fetchrewardscodingexercise2/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.fetchrewardscodingexercise2
+package com.example.fetchrewardscodingexercise
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.fetchrewardscodingexercise2", appContext.packageName)
+        assertEquals("com.example.fetchrewardscodingexercise", appContext.packageName)
     }
 }
--- app/src/test/java/com/example/fetchrewardscodingexercise2/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fetchrewardscodingexercise2/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.fetchrewardscodingexercise2
+package com.example.fetchrewardscodingexercise
 
 import org.junit.Test
 
